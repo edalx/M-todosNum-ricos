01284015db6fe3389edf8bfab7c00186ad8e19c2
edalx <edd_alex.66@hotmail.com>
2016-06-30 00:42:18 -0500

    Add GUI Lineal Systems

--- src/main/java/edalx/methods/EliminacionGaussiana.java
+++ src/main/java/edalx/methods/EliminacionGaussiana.java
@@ -10,25 +10,103 @@ package edalx.methods;
  * @author edalx
  */
 public class EliminacionGaussiana {
-  
-    public Double[] gaussSinPivote(Double[][] matriz, Double[] aumen){   
-    Double[] result=new Double[aumen.length];
-    double m=0;
-        for (int i = 0; i < result.length-1; i++) {
-            for (int k = i+1; k < result.length; k++) {
-                if (matriz[i][i]==0) {
+
+    public Double[] gaussSinPivote(Double[][] matriz, Double[] aumen) {
+        Double[] result = new Double[aumen.length];
+        double m = 0;
+        double aux = 0;
+        for (int i = 0; i < result.length - 1; i++) {
+            for (int k = i + 1; k < result.length; k++) {
+                if (matriz[i][i] == 0) {
                     System.out.println("Existen ceros en la diagonal principal");
-                }else{
-                   
-                m=matriz[k][i]/matriz[i][i];
-                
-                for (int j = 0; j< result.length; j++) {
-                   matriz[k][j]=matriz[k][j]-m*matriz[i][j];
+                    System.exit(0);
+                } else {
+
+                    m = matriz[k][i] / matriz[i][i];
+                    matriz[k][i] = 0.0;
+                    for (int j = i + 1; j < result.length; j++) {
+                        matriz[k][j] = matriz[k][j] - m * matriz[i][j];
+                    }
+
+                    aumen[k] = aumen[k] - m * aumen[i];
+
                 }
-                }
-                
+
             }
         }
-       return null; 
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = 0.0;
+        }
+
+        for (int i = aumen.length - 1; i >= 0; i--) {
+            aux = 0;
+            for (int j = i + 1; j < result.length; j++) {
+                aux = aux + matriz[i][j] * result[j];
+            }
+            result[i] = (aumen[i] - aux) / matriz[i][i];
+        }
+        return result;
+
+    }
+    
+    public Double[] PartialPivoting(Double[][] A, Double[] b){
+        int r=0;
+        double aux1;
+        double aux2;
+        double m = 0;
+        double aux = 0;
+        Double[] result = new Double[b.length];
+        for (int i = 0; i < b.length-1; i++) {
+            r=i;
+            for (int j = i+1; j < b.length; j++) {
+                if (A[j][i]>A[r][j]) {
+                    r=i;
+                }
+            }
+            if (A[r][i]==0) {
+                System.out.println("El sistema no tiene solucion unica");
+            }
+            for (int k = 0; k < b.length; k++) {
+                aux1=A[i][k];
+                A[i][k]=A[r][k];
+                A[r][k]=aux1;
+                
+                aux2=b[r];
+                b[r]=b[i];
+                b[i]=aux2;
+            }
+            
+            for (int k = i + 1; k < result.length; k++) {
+                if (A[i][i] == 0) {
+                    System.out.println("Existen ceros en la diagonal principal");
+                    System.exit(0);
+                } else {
+
+                    m = A[k][i] / A[i][i];
+                    A[k][i] = 0.0;
+                    for (int j = i + 1; j < result.length; j++) {
+                        A[k][j] = A[k][j] - m * A[i][j];
+                    }
+
+                    b[k] = b[k] - m * b[i];
+
+                }
+
+            }
+        }
+        
+        for (int i = 0; i < result.length; i++) {
+            result[i] = 0.0;
+        }
+
+        for (int i = b.length - 1; i >= 0; i--) {
+            aux = 0;
+            for (int j = i + 1; j < result.length; j++) {
+                aux = aux + A[i][j] * result[j];
+            }
+            result[i] = (b[i] - aux) / A[i][i];
+        }
+        return result;
     }
 }
---
+++ src/main/java/edalx/methods/Operaciones.java
@@ -0,0 +1,96 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edalx.methods;
+
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author Alexander
+ */
+public class Operaciones {
+
+    public String[] matrizTokens(String cadena, int filas) {
+        int cont1 = 0;
+        String[] matrizBorrador = new String[filas];
+        StringTokenizer st = new StringTokenizer(cadena, "\n");
+        while (st.hasMoreTokens()) {
+            matrizBorrador[cont1] = st.nextToken();
+            cont1++;
+        }
+        return matrizBorrador;
+
+    }
+
+    /**
+     * Captura la matriz en un arreglo bidimensional
+     *
+     * @param matriz1 matriz de tokens
+     * @param columnas dimension de la nueva matriz
+     * @param filas dimension de la nueva matriz
+     * @return El método devuelve una matriz de tokens convertidos a numeros
+     */
+    public Double[][] capturaMatriz(String[] matriz1, int filas, int columnas) {
+        Double[][] matrizOriginal = new Double[filas][columnas];
+        for (int i = 0; i < filas; i++) {
+            StringTokenizer st1 = new StringTokenizer(matriz1[i], ", ");
+            for (int k = 0; k < columnas; k++) {
+                String num = st1.nextToken();
+                double ste = Double.parseDouble(num);
+                matrizOriginal[i][k] = ste;
+            }
+
+        }
+        return matrizOriginal;
+    }
+
+    public Double[] capturaVector(String[] matriz1, int filas) {
+        Double[] matrizOriginal = new Double[filas];
+        for (int i = 0; i < filas; i++) {
+            matrizOriginal[i] = Double.parseDouble(matriz1[i].trim());
+        }
+        return matrizOriginal;
+    }
+
+    public double[][] multiplicacion(double matriz1[][], double matriz2[][], int f1, int filasColumnas, int c2) {
+        double matriz3[][] = new double[f1][c2];
+        double entrada = 0;
+        for (int i = 0; i < f1; i++) {
+            for (int j = 0; j < c2; j++) {
+                for (int k = 0; k < filasColumnas; k++) {
+                    entrada = entrada + (matriz1[i][k] * matriz2[k][j]);
+                }
+                matriz3[i][j] = entrada;
+                entrada = 0;
+            }
+        }
+        return matriz3;
+    }
+
+    public double[][] suma(double matriz1[][], double matriz2[][], int f1, int c1) {
+        double matriz3[][] = new double[f1][c1];
+        for (int i = 0; i < f1; i++) {
+            for (int j = 0; j < c1; j++) {
+                matriz3[i][j] = matriz1[i][j] + matriz2[i][j];
+            }
+        }
+
+        return matriz3;
+
+    }
+
+    public String[][] capturaVector(String texto, int filas) {
+
+        int cont1 = 0;
+        String[][] matrizBorrador = new String[filas][1];
+        StringTokenizer st = new StringTokenizer(texto, "\n");
+        while (st.hasMoreTokens()) {
+            matrizBorrador[cont1][1] = st.nextToken();
+            cont1++;
+        }
+        return matrizBorrador;
+    }
+
+}
